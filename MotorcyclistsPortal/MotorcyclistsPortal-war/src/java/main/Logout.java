e0a7d467f7bbcacc698e583ce9d6e44960075a47
kalosh <kalosh@0214a254-46e5-11de-b25d-9f9778916a3c>
2008-03-07 10:40:40 +0000

    Dodanie akcji wylogowania.

--- MotorcyclistsPortal/MotorcyclistsPortal-war/src/java/main/Logout.java
+++ MotorcyclistsPortal/MotorcyclistsPortal-war/src/java/main/Logout.java
@@ -7,6 +7,7 @@ package main;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -14,11 +15,13 @@ import org.springframework.web.servlet.mvc.Controller;
  *
  * @author kalosh
  */
-public class IndexController implements Controller{
+public class Logout implements Controller{
 
     public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
-        String message="pupka";
-        return new ModelAndView("index");
+        HttpSession session = arg0.getSession(false);
+        if(null != session)
+            session.invalidate();
+        return new ModelAndView("index", "message", "Wylogowano poprawnie");
     }
 
 }
