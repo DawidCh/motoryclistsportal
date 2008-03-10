fd26046912132f807db26152e507aea1980aac8f
kalosh <kalosh@0214a254-46e5-11de-b25d-9f9778916a3c>
2008-03-10 00:11:57 +0000

    Utworzenie sesyjnego beana i getera beanów.

---
+++ MotorcyclistsPortal/MotorcyclistsPortal-war/src/java/utils/BeanGetter.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utils;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.context.ApplicationContext;
+
+/**
+ *
+ * @author kalosh
+ */
+public class BeanGetter {
+
+    public static Object getBean(String beanId, HttpServletRequest request)
+    {
+         ApplicationContext ac = org.springframework.web.context.support.
+           WebApplicationContextUtils.getRequiredWebApplicationContext(
+                               request.getSession().getServletContext());
+         return ac.getBean(beanId);
+    }
+}
