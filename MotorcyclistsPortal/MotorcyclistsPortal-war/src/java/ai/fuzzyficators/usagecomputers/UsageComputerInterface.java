d0715af0f0f991a993118e3dbb3a5ab1203c00ef
kalosh <kalosh@0214a254-46e5-11de-b25d-9f9778916a3c>
2008-07-05 12:54:03 +0000

    Poprawki logowania oraz przebiegu kodu.

--- MotorcyclistsPortal/MotorcyclistsPortal-war/src/java/ai/fuzzyficators/usagecomputers/UsageComputerInterface.java
+++ MotorcyclistsPortal/MotorcyclistsPortal-war/src/java/ai/fuzzyficators/usagecomputers/UsageComputerInterface.java
@@ -10,5 +10,12 @@ package ai.fuzzyficators.usagecomputers;
  * @author kalosh
  */
 public interface UsageComputerInterface {
-    double computePercentageValue(int partUsageInUnits, int partAvailabilityUsage);
+    /**
+     * Method used for computing how hard part is used (in %).
+     * @param partUsageInUnits current part usage
+     * @param partAvailabilityUsage part availability usage
+     * @return value of usage from range 0-1
+     */
+    double computePercentageValue(int partUsageInUnits,
+            int partAvailabilityUsage);
 }
--- MotorcyclistsPortal/MotorcyclistsPortal-war/src/java/main/IndexController.java
+++ MotorcyclistsPortal/MotorcyclistsPortal-war/src/java/main/IndexController.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.Logger;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 import org.springframework.web.servlet.support.RequestContextUtils;
@@ -19,15 +20,26 @@ import utils.LocaleProvider;
  *
  * @author kalosh
  */
-public class IndexController implements Controller{
+public class IndexController implements Controller {
 
+    /**
+     * Method used for handling main index page.
+     * @param request HTTP request object
+     * @param response HTTP response object
+     * @return ModelAndView object which contains map of object passed to page
+     * and chosen page for display
+     * @throws java.lang.Exception
+     */
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Logger.getLogger("E").trace("Entering to: handleRequest");
         // <editor-fold defaultstate="collapsed" desc="Generated vars: localeProvider, defaultLocale,formInfo and put vars into">
         LocaleProvider localeProvider = BeanGetter.getLocaleProvider(request);
         Locale currentLocale = RequestContextUtils.getLocale(request);
-        HashMap<String, Object> formInfo = new HashMap<String, Object>();
-        formInfo.put("pageTitle", localeProvider.getMessage("index.pageTitle", null, currentLocale));
+        HashMap<String, Object> formInfo = new HashMap < String, Object >();
+        formInfo.put("pageTitle", localeProvider.getMessage("index.pageTitle",
+                null, currentLocale));
         // </editor-fold>
+        Logger.getLogger("E").trace("Exiting from: handleRequest");
         return new ModelAndView("index", formInfo);
     }
 
